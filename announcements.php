d573a120924cfb5c0ad9f467452bcae0e566904e
Mayeoh Barangot <20230028163@my.xu.edu.ph>
2026-05-17 20:12:07 +0800

    Delete announcements.php

--- announcements.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/auth.php';
-startSession();
-if (isLoggedIn()) {
-    header('Location: /dashboard.php');
-} else {
-    header('Location: /login.php');
-}
-exit;
